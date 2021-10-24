f421207d918668074dbd68e4d6fb0e061979349c
Haleem Hosein <haleemhosein@yahoo.com>
2021-10-24 13:12:34 -0400

    03 - Testing Request Validation

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Project;
+use Faker\Generator as Faker;
+
+$factory->define(Project::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'description' => $faker->paragraph,
+    ];
+});
