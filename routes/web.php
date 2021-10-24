19b68beef65c8455470431fb889cb2047e63280c
Haleem Hosein <haleemhosein@yahoo.com>
2021-10-24 12:07:58 -0400

    02 - Let's Begin with a Test

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/projects', 'ProjectsController@index');
+
+Route::post('/projects', 'ProjectsController@store');
