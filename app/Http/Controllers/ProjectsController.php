e7d2cd373e1b7faf24efdfa25b67458bc1b693ae
Haleem Hosein <haleemhosein@yahoo.com>
2021-11-06 19:58:16 -0400

    18 - Reduce Form Duplication

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Project;
-use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
 {
+    /**
+     * View all projects.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $projects = auth()->user()->projects;
@@ -14,6 +18,14 @@ class ProjectsController extends Controller
         return view('projects.index', compact('projects'));
     }
 
+    /**
+     * Show a single project.
+     *
+     * @param Project $project
+     *
+     * @return \Illuminate\Http\Response
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
     public function show(Project $project)
     {
         $this->authorize('update', $project);
@@ -21,30 +33,66 @@ class ProjectsController extends Controller
         return view('projects.show', compact('project'));
     }
 
+    /**
+     * Create a new project.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('projects.create');
+    }
+
+    /**
+     * Persist a new project.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store()
     {
-       $attributes = request()->validate([
-           'title' => 'required',
-           'description' => 'required',
-           'notes' => 'min:3',
-        ]);
-
-        $project = auth()->user()->projects()->create($attributes);
+        $project = auth()->user()->projects()->create($this->validateRequest());
 
         return redirect($project->path());
     }
 
+    /**
+     * Edit the project.
+     *
+     * @param  Project $project
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Project $project)
+    {
+        return view('projects.edit', compact('project'));
+    }
+
+    /**
+     * Update the project.
+     *
+     * @param  Project $project
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
     public function update(Project $project)
     {
         $this->authorize('update', $project);
 
-        $project->update(request(['notes']));
+        $project->update($this->validateRequest());
 
         return redirect($project->path());
     }
 
-    public function create()
+    /**
+     * Validate the request attributes.
+     *
+     * @return array
+     */
+    protected function validateRequest()
     {
-        return view('projects.create');
+        return request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'notes' => 'min:3'
+        ]);
     }
 }
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,12 +1,14 @@
-@extends('layouts.app')
+@extends ('layouts.app')
 
 @section('content')
     <header class="flex items-center mb-3 pb-4">
         <div class="flex justify-between items-end w-full">
             <p class="text-grey text-sm font-normal">
-                <a href="/projects" class="text-grey text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
+                <a href="/projects" class="text-grey text-sm font-normal no-underline hover:underline">My Projects</a>
+                / {{ $project->title }}
             </p>
-            <a class="button" href="/projects/create">New Project</a>
+
+            <a href="{{ $project->path().'/edit' }}" class="button">Edit Project</a>
         </div>
     </header>
 
@@ -14,51 +16,57 @@
         <div class="lg:flex -mx-3">
             <div class="lg:w-3/4 px-3 mb-6">
                 <div class="mb-8">
-                    <h2 class="text-grey font-normal text-lg mb-3">Tasks</h2>
+                    <h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
 
                     {{-- tasks --}}
                     @foreach ($project->tasks as $task)
                         <div class="card mb-3">
-                            <form action="{{ $task->path() }}" method="POST">
+                            <form method="POST" action="{{ $task->path() }}">
                                 @method('PATCH')
                                 @csrf
 
-                                <div class="flex items-center">
+                                <div class="flex">
                                     <input name="body" value="{{ $task->body }}" class="w-full {{ $task->completed ? 'text-grey' : '' }}">
-                                    <input {{ $task->completed ? 'checked' : '' }} type="checkbox" name="completed" onChange="this.form.submit()">
+                                    <input name="completed" type="checkbox" onChange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
                                 </div>
-
                             </form>
                         </div>
                     @endforeach
 
-                    <div class="card mb3">
+                    <div class="card mb-3">
                         <form action="{{ $project->path() . '/tasks' }}" method="POST">
                             @csrf
 
-                            <input name="body" placeholder="Add a new task..." class="w-full">
+                            <input placeholder="Add a new task..." class="w-full" name="body">
                         </form>
                     </div>
                 </div>
 
                 <div>
-                    <h2 class="text-grey font-normal text-lg mb-3">General Notes</h2>
+                    <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
 
                     {{-- general notes --}}
                     <form method="POST" action="{{ $project->path() }}">
                         @csrf
                         @method('PATCH')
 
-                        <textarea name="notes" class="card w-full mb-4" style="min-height: 200px" placeholder="Enter some notes...">{{ $project->notes }}</textarea>
+                        <textarea
+                            name="notes"
+                            class="card w-full mb-4"
+                            style="min-height: 200px"
+                            placeholder="Anything special that you want to make a note of?"
+                        >{{ $project->notes }}</textarea>
 
                         <button type="submit" class="button">Save</button>
                     </form>
                 </div>
             </div>
 
-            <div class="lg:w-1/4 px-3">
+            <div class="lg:w-1/4 px-3 lg:py-8">
                 @include ('projects.card')
             </div>
         </div>
     </main>
+
+
 @endsection
