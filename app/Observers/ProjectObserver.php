96c5a15e6c98a8eefe1239cfe43e9497701e4220
Haleem Hosein <haleemhosein@yahoo.com>
2021-11-09 07:32:49 -0400

    20 - Project Activity Feeds

---
+++ app/Observers/ProjectObserver.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Observers;
+
+use App\Activity;
+use App\Project;
+
+class ProjectObserver
+{
+    /**
+     * Handle the project "created" event.
+     *
+     * @param  \App\Project  $project
+     * @return void
+     */
+    public function created(Project $project)
+    {
+        Activity::create([
+            'project_id' => $project->id,
+            'description' => 'created',
+        ]);
+    }
+
+    /**
+     * Handle the project "updated" event.
+     *
+     * @param  \App\Project  $project
+     * @return void
+     */
+    public function updated(Project $project)
+    {
+        Activity::create([
+            'project_id' => $project->id,
+            'description' => 'updated',
+        ]);
+    }
+
+    /**
+     * Handle the project "deleted" event.
+     *
+     * @param  \App\Project  $project
+     * @return void
+     */
+    public function deleted(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the project "restored" event.
+     *
+     * @param  \App\Project  $project
+     * @return void
+     */
+    public function restored(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the project "force deleted" event.
+     *
+     * @param  \App\Project  $project
+     * @return void
+     */
+    public function forceDeleted(Project $project)
+    {
+        //
+    }
+}
